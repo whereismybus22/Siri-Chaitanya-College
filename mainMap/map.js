ce605e03b3c34758d3a9bc8be41bf73de839330b
21r21a12b7 <21r21a12b7@mlrinstitutions.ac.in>
2025-06-30 19:27:26 +0530

    Update map.js

--- mainMap/map.js
+++ mainMap/map.js
@@ -102,7 +102,7 @@ function calculateDistanceTimeSpeed(locationOne, locationTwo, speed) {
 async function fetchBusLocation() {
 
   // const auth = await hypegpstracker(whereismybus);
-  const auth = 'RzBFAiEAqnthKSztEUxAa2pZG8lhB7LoqTFpv2M46hKWRRTBSB8CIHfHrp3p6VloF3LEYR0DNkskBhwcERic7db5JSy6bzjFeyJ1Ijo2Mzk0MCwiZSI6IjIwMjYtMDYtMjlUMTg6MzA6MDAuMDAwKzAwOjAwIn0';
+  const auth = 'RzBFAiEAj0-z4WfV-q46sVbqWtyzP4Is0YqxBoUNdSYgK3ldxKoCIERag1jrS97AEASdei9LMaiqYBoPymHHpdx7LX6Pf0JGeyJ1Ijo4NDY0MSwiZSI6IjIwMjYtMDYtMjlUMTg6MzA6MDAuMDAwKzAwOjAwIn0';
   const url = 'https://demo.traccar.org/api/positions';
 
   try {
